3705b10b467a2c5a4f57ac5ac74a292b0ca784f8
devinswenson <devin.swenson@affirm.com>
2022-08-22 22:21:59 -0500

    Sort by last_completed timestamp Logic is in place to sort the chores by timestamp. Next step is to allow for it to be updated on button click

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
-import sqlite3
-from flask import Flask, render_template
+import sqlite3, time
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
 # CREATE
+# Future implementation
+
 # READ
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    chores = conn.execute('SELECT * FROM chores').fetchall()
+    conn.close()
+    print("we did it")
+    return render_template('test.html', chores=chores)
+
 # UPDATE
+@app.route('/update')
+def update():
+    id = request.form.get("id")
+    conn = get_db_connection()
+    try:
+        conn.execute('UPDATE chores SET last_performed=' + time.time() + 'WHERE id =' + id)
+        conn.close()
+        return "Success"
+    except:
+        conn.close()
+        return "Failure"
+
 # DELETE
+# future implementation
 
 # SUPPORT
 def get_db_connection():
@@ -19,13 +37,6 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-@app.route('/test')
-def index():
-    conn = get_db_connection()
-    chores = conn.execute('SELECT * FROM chores').fetchall()
-    conn.close()
-    print("we did it")
-    return render_template('test.html', chores=chores)
 
 if __name__ == '__main__':
     app.run()
--- init_db.py
+++ init_db.py
@@ -8,12 +8,24 @@ with open('schema.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO chores (chore_name, frequency) VALUES (?, ?)",
-            ('Clean this', 'd')
+cur.execute("INSERT INTO chores (chore_name, frequency, last_performed) VALUES (?, ?, ?)",
+            ('third', 'd', 1661223595)
+            )
+cur.execute("INSERT INTO chores (chore_name, frequency, last_performed) VALUES (?, ?, ?)",
+            ( 'second', 'd', 1651223595)
+            )
+cur.execute("INSERT INTO chores (chore_name, frequency, last_performed) VALUES (?, ?, ?)",
+            ('Furst', 'd', 1641223595)
             )
 
-cur.execute("INSERT INTO chores (chore_name, frequency) VALUES (?, ?)",
-            ('Clean that', 'w')
+
+
+
+
+
+
+cur.execute("INSERT INTO chores (chore_name, frequency, last_performed) VALUES (?, ?, ?)",
+            ('Clean that', 'w', 1641223595)
             )
 
 connection.commit()
